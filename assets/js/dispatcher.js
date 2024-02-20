ec19437369729a497c7d8262c78b75dbc4f439af
pixplays <107187283+pixplays@users.noreply.github.com>
2024-02-21 00:41:13 +0100

    Populating inlinesLookup properly

--- assets/js/dispatcher.js
+++ assets/js/dispatcher.js
@@ -93,7 +93,7 @@ async function fetchInlines(){
     var inlinesLookup = {};
     for (let i=0; i < entries.length; i++) {
         if (entries[i].inline){
-            inlinesLookup[entries[i].url] = entries[i].inline;
+            inlinesLookup[document.location.origin+entries[i].url] = entries[i].inline;
         }
     }
 
